f7fef31b527e8b11c46bb05fc2f4afaaed0b3f93
JuhoKang <rjduakcjs@gmail.com>
2017-01-04 16:33:48 +0900

    Solve Korean encoding problem

--- src/main/java/kr/ec/queryfly/analyzer/config/WebConfig.java
+++ src/main/java/kr/ec/queryfly/analyzer/config/WebConfig.java
@@ -1,7 +1,9 @@
 package kr.ec.queryfly.analyzer.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -11,12 +13,14 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.nio.charset.Charset;
+import java.util.List;
 import javax.servlet.Filter;
 
 /**
  * Created by juho on 16. 12. 31.
  */
 @Configuration
+@ComponentScan(basePackages = {"kr.ec.queryfly.analyzer.web"})
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
@@ -35,12 +39,21 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     return new StringHttpMessageConverter(Charset.forName("UTF-8"));
   }
 
+  public void configureMessageConverters(
+      List<HttpMessageConverter<?>> converters) {
+
+    converters.add(responseBodyConverter());
+
+    super.configureMessageConverters(converters);
+  }
+
   @Bean
-  public Filter characterEncodingFilter() {
+  public FilterRegistrationBean filterRegistrationBean() {
+    FilterRegistrationBean registrationBean = new FilterRegistrationBean();
     CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
     characterEncodingFilter.setEncoding("UTF-8");
-    characterEncodingFilter.setForceEncoding(true);
-    return characterEncodingFilter;
+    registrationBean.setFilter(characterEncodingFilter);
+    return registrationBean;
   }
 
 }
